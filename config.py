df2dbfbd0d7a8bc869caa29469912b56e6a7abe0
Sevmamra <sevmamra01@gmail.com>
2025-05-17 07:56:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,30 +2,30 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = "7706778428:AAG2uLYZm84_PLP9IFAWiXr6aTOwVkhUBJA"
+    BOT_TOKEN = "7734880223:AAGnNR-dX9tnPWH2gU3xKdCtAhd_zCRghy8"
     
-    API_ID = 12475131
+    API_ID = 26797881
     
-    API_HASH = "719171e38be5a1f500613837b79c536f"
-    ADMIN_ID = [1714266885]
+    API_HASH = "9699262c708c2e45ba18bfce925ed5ed"
+    ADMIN_ID = [6567162029]
     
-    DB_URL = "mongodb+srv://nitinyadav798906:CClOsRu0f90aluAC@cluster0.t6r8x57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://sevmamra01:<db_password>@cluster0.ff29e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     
     DB_NAME = "MY_BOT_DB"
     
-    TXT_LOG = -1002542634912
+    TXT_LOG = -1002303981738
     
-    AUTH_LOG = -1002542634912
+    AUTH_LOG = -1002303981738
     
-    HIT_LOG = -1002542634912
+    HIT_LOG = -1002303981738
     
-    DRM_DUMP = -1002542634912
+    DRM_DUMP = -1002303981738
     # Main channel ki ID
-    CHANNEL = -1002542634912
+    CHANNEL = -1002303981738
     # Channel ka link
-    CH_URL = "https://t.me/+zU2qSJ_kKgZiMDIx"
+    CH_URL = "https://t.me/+8yAoCM55ditkYmM1"
     
-    OWNER = "https://t.me/+zU2qSJ_kKgZiMDIx"
+    OWNER = "https://t.me/+8yAoCM55ditkYmM1"
     
     THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
    
